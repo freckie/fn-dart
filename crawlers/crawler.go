49c3eccdd7f66bbc7007353975c54c4117c9378d
freckie <freckie@frec.kr>
2020-07-05 22:19:27 +0900

    Fix

--- crawlers/crawler.go
+++ crawlers/crawler.go
@@ -4,18 +4,16 @@ import (
 	"fn-dart/models"
 )
 
+const DartPopupURL = "http://dart.fss.or.kr/dsaf001/main.do?rcpNo=%s"
+const DartReportURL = "http://dart.fss.or.kr/report/viewer.do?rcpNo=%s&dcmNo=%s&eleId=0&offset=0&length=0&dtd=HTML"
+
 type Crawler interface {
-	GetCID() string                              // Crawler ID
-	GetList(number int) ([]models.Report, error) // Crawl list of Reports
-	GetDetail(item *models.Report) error         // Crawl detail values of each Reports
+	GetCID() string                      // Crawler ID
+	GetDetail(item *models.Report) error // Crawl detail values of each Reports
 }
 
 func GetCID(c Crawler) string {
 	return c.GetCID()
-}
-
-func GetList(c Crawler, number int) ([]models.Report, error) {
-	return c.GetList(number)
 }
 
 func GetDetail(c Crawler, item *models.Report) error {
