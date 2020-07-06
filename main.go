777dd2271297ad56805e48a75ae1cbad2224d7a4
freckie <freckie@frec.kr>
2020-07-06 17:53:30 +0900

    Update: code for testing

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 
 	"fn-dart/config"
+	"fn-dart/crawlers"
+	"fn-dart/models"
 	"fn-dart/utils"
 )
 
@@ -15,10 +17,38 @@ func main() {
 		os.Exit(0)
 	}
 
-	data, err := utils.GetRecentReports(cfg, "20200703", "1", "30")
+	data, err := utils.GetRecentReports(*cfg, "20200706", "2", "100")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(0)
 	}
-	fmt.Println(data)
+
+	var c crawlers.Crawler
+	cid3 := crawlers.CID3{}
+	cid4 := crawlers.CID4{}
+
+	for _, item := range data {
+		report := models.Report{}
+		report.Title = item.ReportNM
+		report.RceptNo = item.RceptNo
+		report.CorpName = item.CorpName
+
+		if cid3.IsTarget(item.ReportNM) {
+			report.CrawlerID = "3"
+			c = cid3
+		} else if cid4.IsTarget(item.ReportNM) {
+			report.CrawlerID = "4"
+			c = cid4
+		} else {
+			continue
+		}
+
+		err := c.GetDetail(&report)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		fmt.Println(report)
+	}
 }
