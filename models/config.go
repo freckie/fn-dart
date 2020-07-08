6672d9098cc98a7b8de3649739c54a6630206e87
freckie <freckie@frec.kr>
2020-07-08 12:03:11 +0900

    Update: config filestructure

--- models/config.go
+++ models/config.go
@@ -3,7 +3,7 @@ package models
 type Config struct {
 	Telegram ConfigItemTelegram `json:"telegram"`
 	Crawler  ConfigItemCrawler  `json:"crawler"`
-	Targets  []ConfigItemTarget `json:"targets"`
+	Targets  ConfigItemTarget   `json:"targets"`
 }
 
 type ConfigItemTelegram struct {
@@ -18,8 +18,14 @@ type ConfigItemCrawler struct {
 }
 
 type ConfigItemTarget struct {
-	CrawlerID     int    `json:"crawler_id"`
-	On            bool   `json:"on"`
-	Keyword       string `json:"keyword"`
+	CID1 ConfigItemTargetItem `json:"1"`
+	CID2 ConfigItemTargetItem `json:"2"`
+	CID3 ConfigItemTargetItem `json:"3"`
+	CID4 ConfigItemTargetItem `json:"4"`
+}
+
+type ConfigItemTargetItem struct {
+	Description   string `json:"description"`
+	ValuesCount        int    `json:"values_count"`
 	MessageFormat string `json:"message_format"`
 }
