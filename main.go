c20eaaf2fe84f942f9c4c8bef53214e2ffec5988
freckie <freckie@frec.kr>
2020-07-09 22:43:12 +0900

    Fix: now completed main

--- main.go
+++ main.go
@@ -1,82 +1,54 @@
+//go:generate goversioninfo
+
 package main
 
 import (
 	"fmt"
+	"log"
 	"os"
-	"strings"
 
-	"fn-dart/config"
-	"fn-dart/crawlers"
+	"runtime"
+
 	"fn-dart/engine"
-	"fn-dart/models"
-	"fn-dart/utils"
 )
 
+var logger *log.Logger
+
 func main() {
-	cfg, err := config.LoadConfig("config.json")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(0)
-	}
+	// Testmode
+	testMode := false
 
-	data, err := utils.GetRecentReports(*cfg, "", "1", "100")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(0)
-	}
+	if !testMode {
+		// Logger
+		logger = log.New(os.Stdout, "LOG ", log.LstdFlags)
 
-	var c crawlers.Crawler
-	cid1 := crawlers.CID1{}
-	cid2 := crawlers.CID2{}
-	cid3 := crawlers.CID3{}
-	cid4 := crawlers.CID4{}
-
-	tg := engine.TGEngine{}
-	tg.Cfg = cfg
-	err = tg.GenerateBot()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(0)
-	}
-
-	for _, item := range data {
-		report := models.Report{}
-		report.Title = item.ReportNM
-		report.RceptNo = item.RceptNo
-		report.CorpName = item.CorpName
-
-		if strings.Contains(item.ReportNM, "[기재정정]") {
-			continue
-		}
-
-		if cid1.IsTarget(item.ReportNM) {
-			report.CrawlerID = "1"
-			c = cid1
-		} else if cid2.IsTarget(item.ReportNM) {
-			report.CrawlerID = "2"
-			c = cid2
-		} else if cid3.IsTarget(item.ReportNM) {
-			report.CrawlerID = "3"
-			c = cid3
-		} else if cid4.IsTarget(item.ReportNM) {
-			report.CrawlerID = "4"
-			c = cid4
+		// Get config file path from os.Args
+		var configPath string
+		args := os.Args
+		if len(args) <= 1 {
+			configPath = "config.json"
 		} else {
-			continue
+			configPath = args[1]
 		}
 
-		err := c.GetDetail(&report)
+		// Engine
+		en := engine.Engine{}
+		err := en.Init(logger, configPath)
 		if err != nil {
-			fmt.Println(err)
-			continue
+			logger.Println("[INIT ERROR]", err)
+			fmt.Scan()
+			os.Exit(0)
 		}
 
-		err = tg.SendMessage(report)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
+		// Parallel Processing
+		runtime.GOMAXPROCS(en.Cfg.Crawler.MaxProcs)
 
-		fmt.Println(report)
+		en.Run()
+		fmt.Scan()
+		os.Exit(0)
+	} else {
+
+		/* Write Test Code Here */
+
 	}
 }
